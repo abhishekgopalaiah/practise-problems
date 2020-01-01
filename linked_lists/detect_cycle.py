9cf0dc22e0e5d98e8c2d170dfc04c891eedf505a
agopalaiah <agopalaiah@quotient.com>
2020-01-01 11:58:57 +0530

    detect cycle

--- linked_lists/detect_cycle.py
+++ linked_lists/detect_cycle.py
@@ -13,13 +13,18 @@ Explanation
 The first list has no cycle, so we return false and the hidden code checker prints  to stdout.
 The second list has a cycle, so we return true and the hidden code checker prints  to stdout.
 """
+
+
 def has_cycle(head):
     slow = head
     fast = head
     if not head:
         return 0
-    while slow and fast:
+    while slow and fast and fast.next:
         slow = slow.next
         fast = fast.next.next
         if slow == fast:
             return 1
+
+    return 0
+
