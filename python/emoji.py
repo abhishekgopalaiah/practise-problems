d7009ce54fe08d9d355f3c3d96fc085c1544748e
agopalaiah <agopalaiah@quotient.com>
2020-03-07 08:38:35 +0530

    print emojis

--- python/emoji.py
+++ python/emoji.py
@@ -1,16 +1,22 @@
 """
 print emojis
 """
-print("enter your message")
-message = input().split(" ")
 
-emojis = {
-    ":)": "😄",
-    "(": "😕",
-}
 
-display = ""
-for word in message:
-    display = display + emojis.get(word, word) + " "
+def display_msg():
+    print("enter your message:\n")
+    message = input().split(" ")
 
-print(display)
+    emojis = {
+        ":)": "😄",
+        "(": "😕",
+    }
+
+    display = ""
+    for word in message:
+        display = display + emojis.get(word, word) + " "
+
+    print(display)
+
+
+display_msg()
